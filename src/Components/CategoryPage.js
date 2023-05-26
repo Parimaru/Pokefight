5e0f896f9652577707eda9e9cdf4f8c233e88e2f
Stefan Lemke <stban@posteo.de>
2023-05-26 11:36:55 +0200

    implemented filter function to map pokemon types with categories

--- src/Components/CategoryPage.js
+++ src/Components/CategoryPage.js
@@ -7,29 +7,34 @@ import PokemonPopover from "./PokemonPopover";
 
 export default function Category() {
   const { pokemons } = useContext(DataContext);
-  //const pokemons = ["Pikatchu", "Ratzfratz", "Pumeluff"];
   const [popover, setPopover] = useState(false);
+  const { category } = useParams();
+  // new array with pokemon type filter for displaying pokemons in corresponding categories
+  const pokemonTypeArray = pokemons?.filter(
+    (pokemon) => pokemon?.type[0] === category || pokemon?.type[1] === category
+  );
 
   const handleClick = () => {
     console.log("I'm the handleClick");
     setPopover(true);
   };
 
-  // useEffect (() => {
-  //   handleClick()
-  // }, [])
+  console.log("category page pokemons", pokemons);
+  console.log("Filtered typeArray ", pokemonTypeArray);
+  console.log("useparams, category", typeof category);
 
-  const { category } = useParams();
   return (
     <>
       <h3>{category}</h3>
       <Box sx={{ flexWrap: "wrap" }}>
         <Grid container spacing={1}>
           <Grid container item spacing={3}>
-            {pokemons?.map((pokemon) => (
+            {pokemonTypeArray?.map((pokemon) => (
               <Grid item xs={2}>
                 <Card onClick={handleClick}>
-                  <Box sx={{ width: 300, height: 200 }}>{pokemon.name}</Box>
+                  <Box sx={{ width: 300, height: 200 }}>
+                    {pokemon.name.english}
+                  </Box>
                 </Card>
               </Grid>
             ))}
