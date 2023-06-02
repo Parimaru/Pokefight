84556a9d5de4633d9c7cf8a692d399bf90e355e2
Timuzzo <t.myasnikov@gmail.com>
2023-06-02 22:15:23 +0200

    random enemy

--- src/Context/DataContext.js
+++ src/Context/DataContext.js
@@ -4,13 +4,12 @@ import { useState, useEffect, createContext } from "react";
 export const DataContext = createContext();
 
 export default function DataContextProvider(props) {
-  const [pokemons, setPokemons] = useState(); // figure out what the resolve of the promise giving back as datatype
-  const [hero, setHero] = useState();
+  const [pokemons, setPokemons] = useState();
+  const [hero, setHero] = useState(true);
   const [enemy, setEnemy] = useState();
   const fetchPokemons = async () => {
     const url =
       "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
-    //const url = "https://pokeapi.co/api/v2/pokemon";
     const response = await fetch(`${url}`);
     const data = await response.json();
     setPokemons(data);
