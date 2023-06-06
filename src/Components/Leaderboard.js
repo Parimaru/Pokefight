24ab633760200559197b04843af7ab6610de4a50
Timuzzo <t.myasnikov@gmail.com>
2023-06-06 09:19:26 +0200

    Leaderboard feature

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,11 +1,28 @@
-export default function Leaderboard () {
+import { useEffect, useState } from "react"
 
+export default function Leaderboard () {
+    const [data, setData] = useState(null)
+
+    async function getAllFighters () {
+        const data = await fetch(`http://localhost:8000/fighters`)
+        const res = await data.json()
+        setData(res.data)
+        console.log("getAllFighters", res)
+    }
+useEffect(()=>{
+    getAllFighters ()
+}, [])
 
     return (
         <>
-        <div>
-            <p>blabla</p>
-        </div>
+            {data?.map((poke) => (
+            <div key={poke._id}>
+            <p>POKEMON: {poke.name}</p>
+            <p>WINS: {poke.wins}</p>
+            <p>LOSES: {poke.loses}</p> 
+            </div>
+            )
+        )}
         </>
     )
 }
